29547c37f9fcb649560f39e2e0ec083364704482
sirlucast <jlmax.torres@gmail.com>
2024-02-08 19:35:49 -0300

    chore: add Getters for User model. Add loginResponse Model

--- internal/users/models/models.go
+++ internal/users/models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"bikesRentalAPI/internal/helpers"
+	"fmt"
 	"time"
 )
 
@@ -9,15 +11,58 @@ type User struct {
 	ID             int64
 	Email          string
 	HashedPassword string
-	FirstName      string
-	LastName       string
+	FirstName      *string
+	LastName       *string
 	CreatedAt      time.Time
 	UpdatedAt      time.Time
+}
+
+func (u *User) FullName() string {
+	return *u.FirstName + " " + *u.LastName
+}
+
+func (u *User) SetPassword(password string) error {
+	hash, err := helpers.GetHashPassword(password)
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %v", err)
+	}
+	u.HashedPassword = hash
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	return helpers.CheckPassword(u.HashedPassword, password)
+}
+
+func (u *User) GetID() int64 {
+	return u.ID
+}
+
+func (u *User) GetFirstName() string {
+	if u.FirstName == nil {
+		return ""
+	}
+	return *u.FirstName
+}
+
+func (u *User) GetLastName() string {
+	if u.LastName == nil {
+		return ""
+	}
+	return *u.LastName
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
 }
 
 type LoginUserRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LoginUserResponse struct {
+	Token string
 }
 
 type CreateUserRequest struct {
