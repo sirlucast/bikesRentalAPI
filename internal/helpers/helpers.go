e6b691d690c615f2a460c193357f8d37c1665296
sirlucast <jlmax.torres@gmail.com>
2024-02-09 02:44:56 -0300

    chore: remove unused code

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -7,18 +7,9 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
 
-	_ "github.com/joho/godotenv/autoload"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func SafeGetEnv(key string) string {
-	if os.Getenv(key) == "" {
-		log.Fatalf("The environment variable '%s' doesn't exist or is not set", key)
-	}
-	return os.Getenv(key)
-}
 
 func WriteJSON(rw http.ResponseWriter, status int, data interface{}) error {
 	js, err := json.Marshal(data)
